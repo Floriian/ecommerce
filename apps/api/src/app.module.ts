b5a0cac87a10ff7bf0854335b944f5b0ce04ba45
Floriian <lovasifloriian@outlook.hu>
2023-06-05 00:30:08 +0200

    api: connect app to mongo.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,11 +5,14 @@ import { ProductsModule } from './products/products.module';
 import { ConfigModule } from '@nestjs/config';
 import { configModuleOptions } from './config/config';
 import { graphQLConfig } from './config/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+import { mongooseOptions } from './config/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot(configModuleOptions),
     GraphQLModule.forRoot<ApolloDriverConfig>(graphQLConfig),
+    MongooseModule.forRootAsync(mongooseOptions),
     ProductsModule,
   ],
 })
