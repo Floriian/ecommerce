7d9337d3f2f9268eefec0bf696320adc0c79f970
Floriian <lovasifloriian@outlook.hu>
2023-06-05 16:32:39 +0200

    api: generate products module.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { apolloConfig, mongooseConfig } from './config';
 import { ManufacturerModule } from './manufacturer/manufacturer.module';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync(mongooseConfig),
     GraphQLModule.forRoot(apolloConfig),
     ManufacturerModule,
+    ProductsModule,
   ],
 })
 export class AppModule {}
