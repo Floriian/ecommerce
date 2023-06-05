61eeb03c365d3c61ff9f537c6b4e11efc4d219b5
Floriian <lovasifloriian@outlook.hu>
2023-06-05 17:38:44 +0200

    api: findone, findbyname, update,remove on product module.

--- apps/api/src/products/products.service.ts
+++ apps/api/src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductInput } from './dto/create-product.input';
 import { UpdateProductInput } from './dto/update-product.input';
 import { InjectModel } from '@nestjs/mongoose';
@@ -30,15 +30,42 @@ export class ProductsService {
     return await this.productModel.find().populate('manufacturer');
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string): Promise<Product> {
+    const product = await this.productModel
+      .findById(id)
+      .populate('manufacturer');
+
+    if (!product) {
+      throw new NotFoundException("This product doesn't exists.");
+    }
+
+    return product;
   }
 
-  update(id: number, updateProductInput: UpdateProductInput) {
-    return `This action updates a #${id} product`;
+  async update(
+    id: string,
+    updateProductInput: UpdateProductInput,
+  ): Promise<Product> {
+    await this.findOne(id);
+    const manufacturer = await this.manufacturerService.findOne(
+      updateProductInput.manufacturerId,
+    );
+    const update = await this.productModel.findByIdAndUpdate(id, {
+      $set: {
+        ...updateProductInput,
+        ...manufacturer,
+      },
+    });
+
+    return update;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string): Promise<{ success: boolean }> {
+    await this.findOne(id);
+    const del = await this.productModel.findByIdAndDelete(id);
+    if (!del) return { success: false };
+    return {
+      success: true,
+    };
   }
 }
