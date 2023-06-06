474618b47928356d009f3e965939138018599c23
Floriian <lovasifloriian@outlook.hu>
2023-06-06 18:15:22 +0200

    userui: craete product card

--- apps/user/src/components/product/ProductCard.tsx
+++ apps/user/src/components/product/ProductCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import placeholder from "../../assets/placeholder.png";
 import { TProduct } from "@ecommerce/types";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   product: TProduct;
@@ -8,6 +9,11 @@ type Props = {
 
 export function ProductCard({ product }: Props) {
   const img = product.image === "" ? placeholder : product.image;
+
+  const navigate = useNavigate();
+
+  const handleClick = (id: string) => navigate(`/product/${id}`);
+
   return (
     <div className="max-w-[300px] border-2 rounded-md flex flex-col w-full md:w-[576px] p-2 gap-4 justify-between">
       <div>
@@ -24,9 +30,17 @@ export function ProductCard({ product }: Props) {
         <p className="text-gray-800">{product.description}</p>
       </div>
 
-      <button className="bg-green-500 text-white p-2 text-xl rounded-md hover:bg-green-700 transition-colors duration-150">
-        Add to cart
-      </button>
+      <div className="flex w-full gap-4 justify-center">
+        <button className="bg-green-500 text-white p-2 text-xl rounded-md hover:bg-green-700 transition-colors duration-150">
+          Add to cart
+        </button>
+        <button
+          className="bg-blue-500 text-white p-2 text-xl rounded-md hover:bg-blue-700 transition-colors duration-150"
+          onClick={() => handleClick(product._id)}
+        >
+          See more
+        </button>
+      </div>
     </div>
   );
 }
