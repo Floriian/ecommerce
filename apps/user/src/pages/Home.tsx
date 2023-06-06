c41077a514c9d35bcd0b19855a5cb563e0adc580
Floriian <lovasifloriian@outlook.hu>
2023-06-06 13:52:40 +0200

    userui: fetch data from backend

--- apps/user/src/pages/Home.tsx
+++ apps/user/src/pages/Home.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_MANUFACTURERS } from "../gql";
 
 export default function Home() {
-  return <div>Home</div>;
+  const { loading, data } = useQuery(GET_MANUFACTURERS);
+
+  if (loading) return <h1>Loading...</h1>; //TODO
+
+  return (
+    <div className="">
+      {data?.manufacturers.map((man) => (
+        <p key={man._id}>{man.name}</p>
+      ))}
+    </div>
+  );
 }
