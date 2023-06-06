97fe41d28d4d83a3ccf0c20ceeddef6b82df56b5
Floriian <lovasifloriian@outlook.hu>
2023-06-06 14:05:55 +0200

    api: fix dto.

--- packages/types/src/manufacturer/CreateManufacturerDto.ts
+++ packages/types/src/manufacturer/CreateManufacturerDto.ts
@@ -1,10 +1,15 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString, IsUrl } from "class-validator";
 
 export interface ICreateManufacturerDto {
   name: string;
+  image: string;
 }
 export class CreateManuFacturerDto implements ICreateManufacturerDto {
   @IsString()
   @IsNotEmpty()
   name: string;
+
+  @IsUrl()
+  @IsNotEmpty()
+  image: string;
 }
