12ea54ecb851691e06166b534aa3ae4b353da1fa
Floriian <lovasifloriian@outlook.hu>
2023-06-06 14:15:35 +0200

    api: manufacturer JSDOC.

--- apps/api/src/manufacturer/manufacturer.resolver.ts
+++ apps/api/src/manufacturer/manufacturer.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { ManufacturerService } from './manufacturer.service';
 import { Manufacturer } from './entities/manufacturer.entity';
 import { CreateManufacturerInput } from './dto/create-manufacturer.input';
@@ -8,6 +8,11 @@ import { UpdateManufacturerInput } from './dto/update-manufacturer.input';
 export class ManufacturerResolver {
   constructor(private readonly manufacturerService: ManufacturerService) {}
 
+  /**
+   * It handle 'create' method.
+   * @param createManufacturerInput Incoming request
+   * @returns Manufacturer
+   */
   @Mutation(() => Manufacturer)
   createManufacturer(
     @Args('createManufacturerInput')
@@ -15,22 +20,43 @@ export class ManufacturerResolver {
   ) {
     return this.manufacturerService.create(createManufacturerInput);
   }
+  /**
+   * Query database to find all manufacturers.
+   * @returns Manufacturer[]
+   */
 
   @Query(() => [Manufacturer], { name: 'manufacturers' })
   findAll() {
     return this.manufacturerService.findAll();
   }
 
+  /**
+   * Get one manufacturer by ID.
+   * @param id Manufacturer ID
+   * @returns Manufacturer
+   */
+
   @Query(() => Manufacturer, { name: 'manufacturer' })
   findOne(@Args('id', { type: () => String }) id: string) {
     return this.manufacturerService.findOne(id);
   }
+
+  /**
+   * Get one manufacturer by name.
+   * @param name Manufacturer name
+   * @returns Manufacturer
+   */
 
   @Query(() => Manufacturer, { name: 'manufacturer' })
   findOneByName(@Args('name', { type: () => String }) name: string) {
     return this.manufacturerService.findOneByName(name);
   }
 
+  /**
+   * It updates a manufacturer by ID
+   * @param updateManufacturerInput Incoming request
+   * @returns Manufacturer
+   */
   @Mutation(() => Manufacturer)
   updateManufacturer(
     @Args('updateManufacturerInput')
@@ -41,6 +67,12 @@ export class ManufacturerResolver {
       updateManufacturerInput,
     );
   }
+
+  /**
+   * It deletes a manufacturer by ID. Doesn't include manufacturer products.
+   * @param id Manufacturer ID
+   * @returns { success: boolean }
+   */
 
   @Mutation(() => Manufacturer)
   removeManufacturer(@Args('id', { type: () => String }) id: string) {
