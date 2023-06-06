a399ad94b626231fe2d35e4b728c0a4c77a4368d
Floriian <lovasifloriian@outlook.hu>
2023-06-06 18:15:04 +0200

    barrelexport

--- apps/user/src/components/product/index.ts
+++ apps/user/src/components/product/index.ts
@@ -1,1 +1,2 @@
+export * from "./InterestedProduct";
 export * from "./ProductCard";
--- apps/user/src/gql/index.ts
+++ apps/user/src/gql/index.ts
@@ -1,2 +1,3 @@
 export * from "./getManufacturers";
+export * from "./getOneProduct";
 export * from "./getProducts";
