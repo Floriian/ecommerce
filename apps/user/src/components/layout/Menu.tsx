d7924d4f3280f52748b05710705197d49b54114c
Floriian <lovasifloriian@outlook.hu>
2023-06-07 17:39:00 +0200

    userui: sticky navbar.

--- apps/user/src/components/layout/Menu.tsx
+++ apps/user/src/components/layout/Menu.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Menu() {
   return (
-    <nav className="flex w-full justify-between gap-2 bg-blue-400 bg-opacity-90 p-2 rounded-b-lg">
+    <nav className="flex w-full justify-between gap-2 bg-blue-400 bg-opacity-90 p-2 rounded-b-lg sticky top-0">
       <div>LOGO</div>
       <div className="flex gap-2">
         {[1, 2, 3, 4, 5].map((_, i) => (
